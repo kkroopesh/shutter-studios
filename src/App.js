dfd229667de19431033084704267b53d8c74d6be
kkroopesh <kkroopesh94@gmail.com>
2020-09-19 19:52:30 +0530

    packages + links added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/nav-bar";
+import ContactUs from "./pages/contact-us";
+import Home from "./pages/home";
+import Services from "./pages/services";
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+      <Route path="/contact-us">
+            <ContactUs />
+          </Route>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
